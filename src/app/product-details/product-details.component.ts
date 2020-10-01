c3f74354ce098fceabda42072fed2d406ccabf19
leslieeshea <leslieyshea@gmail.com>
2020-10-01 13:59:11 -0700

    added ActivatedRoute

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
 
   constructor() { }
+    private route: ActivatedRoute;
 
   ngOnInit() {
   }
-
 }
