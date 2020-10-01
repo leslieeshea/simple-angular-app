e52ee548f02ebff33de20565c2a0284f6d56fc4b
leslieeshea <leslieyshea@gmail.com>
2020-10-01 14:26:47 -0700

    integrated routing to the product details page

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { products } from '../products';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -9,9 +11,13 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor() { }
-    private route: ActivatedRoute;
-
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+  
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    });
   }
 }
